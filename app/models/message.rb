a6447ea7ea166f81df9a93695b496345e48ad579
komadorihouse <komadorihouse@yahoo.co.jp>
2021-05-13 05:08:21 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,11 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+  #unless: :was_attached?で条件分岐させている。
+
+  def was_attached?
+    self.image.attached?
+  #画像が存在しなければテキストが必要となり、画像があればテキストは不要になる
+  end
 end
